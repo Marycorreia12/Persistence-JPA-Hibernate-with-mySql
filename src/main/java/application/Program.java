5805edb9840b694d905fb1de25e96d4f3d7313d8
Marycorreia12 <correia_nety@hotmail.com>
2020-09-29 19:12:09 +0100

    Persistence class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,15 +10,20 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Pessoa p1 = em.find(Pessoa.class, 2);
-		System.out.println(p1);
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
 		
 		System.out.println("Pronto");
-		em.close();
-		emf.close();
 	}
 
 }
